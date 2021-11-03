044d56fa37bfb658b0c5b72aa42b0c8f31aa4f41
Khubaib <khubaib.dev@gmail.com>
2021-11-04 04:00:19 +0500

    TS definitions configured

--- src/index.esm.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-var Button = function (_a) {
-    var onClick = _a.onClick, children = _a.children;
-    return React.createElement("button", { onClick: onClick }, children);
-};
-
-export { Button };
-//# sourceMappingURL=index.esm.js.map
