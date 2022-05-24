295c3ceac514a3fc38137ee6c7dbd4fcbe72d4b8
Khubaib <khubaib.dev@gmail.com>
2022-05-24 16:42:47 +0500

    Fix Tab scroll UI

--- src/components/navigation/Tabs/types.ts
+++ src/components/navigation/Tabs/types.ts
@@ -2,6 +2,7 @@ import { TabProps, TabsProps } from "@mui/material";
 
 export interface iTabProps extends Omit<TabProps, "label"> {
   label: string;
+  maxWidth?: number;
 }
 
 export interface iTabsProps extends TabsProps {}
