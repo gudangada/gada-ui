c8ffa35e88d81a69721a1920889a653fef72c1df
Muhammad Khubaib Qaiser <khubaib.qaiser@gudangada.com>
2022-08-02 18:25:55 +0500

    Add Label to Searchbar and improve Menu component (#50)

--- src/components/navigation/Menu/Menu.tsx
+++ src/components/navigation/Menu/Menu.tsx
@@ -3,8 +3,9 @@ import { Menu as MuiMenu } from "@mui/material";
 import { iMenuProps } from "./types";
 import MenuItem from "./MenuItem";
 
-const Menu: React.VFC<iMenuProps> = ({
+const Menu: React.FC<iMenuProps> = ({
   menuItems,
+  children,
   handleClose,
   ...menuProps
 }) => {
@@ -18,12 +19,16 @@ const Menu: React.VFC<iMenuProps> = ({
         vertical: "top",
         horizontal: "center",
       }}
+      BackdropProps={{ style: { backgroundColor: "rgba(0,0,0,0.001)" } }}
+      PaperProps={{ elevation: 1 }}
       {...menuProps}
       onClose={handleClose}
     >
-      {menuItems.map((menuItem) => {
-        return <MenuItem key={menuItem.label} {...menuItem} />;
-      })}
+      {menuItems
+        ? menuItems.map((menuItem) => {
+            return <MenuItem key={menuItem.label} {...menuItem} />;
+          })
+        : children}
     </MuiMenu>
   );
 };
