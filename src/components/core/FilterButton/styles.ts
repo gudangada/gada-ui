47bf89336ddccbce946a18d1f6531149a0fce6e6
Muhammad Khubaib Qaiser <khubaib.qaiser@gudangada.com>
2022-08-03 09:29:16 +0500

    Fix FilterButton hover UI (#52)

--- src/components/core/FilterButton/styles.ts
+++ src/components/core/FilterButton/styles.ts
@@ -5,13 +5,16 @@ import { Button } from "../Button";
 export const StyledFilterButton = styled(Button)<{ isActive: boolean }>(
   ({ isActive, theme: { palette } }) => ({
     [`&.${buttonClasses.root}>*`]: {
-      color: `${palette.interface.teal[700]} !important`,
+      color: `${palette.interface.teal[900]} !important`,
     },
     [`&.${buttonClasses.root}`]: {
       backgroundColor: isActive
         ? palette.interface.teal[100]
         : palette.interface.white,
       border: `1px solid ${palette.interface.teal[700]}`,
+    },
+    [`&.${buttonClasses.root}:hover`]: {
+      backgroundColor: palette.interface.teal[50],
     },
   })
 );
