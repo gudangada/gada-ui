5134631863961edb99a24857dd94cbf27ca8d06f
William Joseph <84491621+williamjosephgada@users.noreply.github.com>
2023-02-13 15:18:31 +0700

    refactor: add new children props on theme provider (#62)

--- src/components/core/ThemeProvider/types.ts
+++ src/components/core/ThemeProvider/types.ts
@@ -1,6 +1,7 @@
 import { iSnackbarProviderProps } from "../../feedback";
 
 export interface iGadaThemeProviderProps {
+  children: any;
   withSnackbarProvider?: boolean;
   snackbarProps?: iSnackbarProviderProps;
 }
